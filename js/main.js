5cc67f49739c070751d7fef493f90974fe252bba
Unknown <lorenzo.palumbo@hotmail.com>
2020-03-21 15:23:50 +0100

    commit2

--- js/main.js
+++ js/main.js
@@ -3,8 +3,16 @@ $(document).ready(function(){
     var template = Handlebars.compile(source);
 
     $('.search').click(function(){
-        var filmInput = $('.input-film').val();
+        cerca();
+    });
+    $('.input-film').keypress(function(event){
+        if (event.key == 'Enter') {
+            cerca();
+        }
+    });
 
+    function cerca(){
+        var filmInput = $('.input-film').val();
         $.ajax({
             url: 'https://api.themoviedb.org/3/search/movie',
             data:{
@@ -17,12 +25,15 @@ $(document).ready(function(){
                 var films = data.results;
                 for (var i = 0; i < films.length; i++) {
                     var film = films[i];
+
                     var context = {
                         titolo: film.title,
                         originale: film.original_title,
-                        lingua: film.original_language,
-                        voto:film.vote_average
+                        lingua: flags(film.original_language),
+                        language: film.original_language,
+                        stelle: stars(film.vote_average)
                      };
+
                      var html = template(context);
                      $('.films-trovati').append(html);
                 }
@@ -31,5 +42,38 @@ $(document).ready(function(){
                 alert('error')
             }
         });
-    });
+    };
+    //Funzione per mettere bandiera in lingua
+    function flags(language){
+        // console.log(language);
+        var lingue = ['ad', 'ae', 'af', 'ag', 'ai', 'al', 'an', 'am', 'ao', 'aq', 'ar', 'as', 'at', 'au', 'aw', 'ax', 'az', 'eu', 'bn', 'dz', 'bh', 'bi', 'br', 'bg', 'my', 'be', 'km', 'ca', 'zh', 'co', 'hr', 'cs', 'da', 'nl', 'eo', 'et', 'fo', 'fa', 'fj', 'fi', 'fr', 'fy', 'gd', 'gv',
+         'gl', 'ka', 'de', 'el', 'kl', 'gn', 'gu', 'ht', 'ha', 'he', 'iw', 'hi', 'hu', 'is', 'io', 'id', 'in', 'ia', 'ie', 'iu', 'ik', 'ga', 'it', 'ja', 'jv', 'kn', 'ks', 'kk', 'rw', 'ky', 'rn', 'ko', 'ku', 'lo', 'la', 'lv', 'li', 'ln', 'lt', 'mk',
+          'mg', 'ms', 'ml', 'mt', 'mi', 'mr', 'mo', 'mn', 'na', 'ne', 'no', 'oc', 'or', 'om', 'ps', 'pl', 'pt', 'pa', 'qu', 'rm', 'ro', 'ru', 'sm', 'sg', 'sa', 'sr', 'st', 'tn', 'sn', 'ii', 'sd', 'si', 'ss', 'sk', 'sl', 'so', 'es', 'su', 'sw', 'sv',
+           'tl', 'tg', 'ta', 'tt', 'te', 'th', 'bo', 'ti', 'to', 'ts', 'tr', 'tk', 'tw', 'ug', 'uk', 'ur', 'uz', 'vi', 'vo', 'wa', 'cy', 'wo', 'xh', 'yi', 'ji', 'yo', 'zu'];
+        // for (var i = 0; i < lingue.length; i++) {
+            if (lingue.includes(language)) {
+
+            }else {
+                htmlLingua = language;
+            
+            }
+    //     };
+        return htmlLingua
+    };
+
+    //Creazione funzione per stelle in maniera dinamica
+    function stars(voto){
+        var cinque = voto/2;
+        var stelle = Math.ceil(cinque);
+
+        var htmlStelle = "";
+        for(var s = 1; s <= 5; s++) {
+            if (s <= stelle) {
+                htmlStelle += '<i class="fas fa-star"></i>';
+            } else {
+                htmlStelle += '<i class="far fa-star"></i>';
+            }
+        }
+        return htmlStelle
+    };
 });
